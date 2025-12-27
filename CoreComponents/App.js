8c25544d67f791db02dbd3cd2c46f92be19cffcb
puran kumar <mylarmpuran@gmail.com>
2025-12-27 23:49:45 +0530

    first commit

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,8 +1,20 @@
-import { View } from 'react-native'
+import { View, Text} from "react-native";
 
 
-export default function App(){
-  return <View style={{ flex: 1, backgroundColor: "plum"}}>
-
-  </View>
+export default function App() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "plum" }}>
+      <View
+        style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
+      ></View>
+      <View style={{
+        width:200, height:200, backgroundColor:"green", padding:60
+      }}>
+          <Text>
+            <Text style={{ color: "white"}}>Hello</Text>
+            Hello world
+          </Text>
+      </View>
+    </View>
+  );
 }
