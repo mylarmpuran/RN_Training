28793ffb882912c076834dcaae68a35f7b79b0d6
puran kumar <mylarmpuran@gmail.com>
2025-12-28 00:34:15 +0530

    scroll view

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,20 +1,69 @@
-import { View, Text} from "react-native";
-
+import { View, Text, Image, ImageBackground, ScrollView } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <View
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <ScrollView>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Text>
+      {/* <View
         style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
-      ></View>
-      <View style={{
-        width:200, height:200, backgroundColor:"green", padding:60
-      }}>
-          <Text>
-            <Text style={{ color: "white"}}>Hello</Text>
-            Hello world
-          </Text>
-      </View>
+      ></View> */}
+      {/* <View
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: "green",
+          padding: 60,
+        }}
+      >
+      
+        <Text>
+          <Text style={{ color: "white" }}>Hello</Text>
+          Hello world
+        </Text>
+      </View> */}
+      <Image source={logoImg} style={{ width: 300, height: 400 }} />
+     
+      <ImageBackground source={logoImg} style={{ flex: 1 }}>
+        <Text>Image TExt</Text>
+      </ImageBackground>
+        <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Text>
+       <Image
+        source={{ uri: "https://i.pinimg.com/736x/ac/73/09/ac7309ea20a637ae031642b539350e11.jpg"}}
+        style={{ width: 300, height: 300 }}
+      />
+      </ScrollView>
     </View>
   );
 }
